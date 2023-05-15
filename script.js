427036d87fe4beb346f38b4cbad6138722cecd77
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-15 14:51:04 +0200

    added basic html structure + initialized variables in js

---
+++ script.js
@@ -0,0 +1,10 @@
+//! Scrivere un programma che chieda all'utente:
+//!     - il numero di chilometri da percorrere
+//!     - eta' del passeggero
+//! Sulla base di qeuste informazioni dovra' calcolare il prezzo totale del biglietto di viaggio secondo le seguenti regole:
+//!     - il prezzo totale del biglietto e' definito in base ai km (0.233 al km)
+//!     - va applicato uno sconto del 19.4% per i minorenni
+//!     - va applicato uno sconto del 37.7% per gli over 65
+
+const quantityKm = document.getElementById('trip-kilometers');
+const passengerAge = document.getElementById('passenger-age');
