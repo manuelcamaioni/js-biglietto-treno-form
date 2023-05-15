a420b3bcecba4349dc4cb4cfd0b354b33e521e5b
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-15 21:06:01 +0200

    initialized variables in function + error check

--- script.js
+++ script.js
@@ -6,31 +6,40 @@
 //!     - va applicato uno sconto del 19.4% per i minorenni
 //!     - va applicato uno sconto del 37.7% per gli over 65
 
-const quantityKm = parseInt(document.getElementById('trip-kilometers').value);
-const passengerAge = parseInt(document.getElementById('passenger-age').value);
+
 const button = document.querySelector('button');
 
-let totalPrice = quantityKm * 0.233;
 // console.log(totalPrice);
-let outputTotalPrice = document.querySelector('span');
-
-let buttonClicked = false;
 
 button.addEventListener('click', function() {
+
+    let quantityKm = parseInt(document.getElementById('trip-kilometers').value);
+    let passengerAge = parseInt(document.getElementById('passenger-age').value);
+
+    let totalPrice = quantityKm * 0.233;
+
+    let outputTotalPrice = document.querySelector('span');
+    
+
+
         if(passengerAge < 18){
             totalPrice = totalPrice - ((20 / 100) * totalPrice);
         }else if(passengerAge >= 65){
             totalPrice = totalPrice - ((40 / 100) * totalPrice);
-        }
-        outputTotalPrice.innerHTML = (' ' + totalPrice.toFixed(2) + ' ' + '€');
+        }else if(isNaN(passengerAge) || isNaN(quantityKm)){
 
-        button.addEventListener('click', function() {
-            buttonClicked = true;
-            if(buttonClicked == true && outputTotalPrice != ''){
-                window.alert('Prezzo già calcolato');
-            }
-            
-        }); 
+            window.alert('I dati inseriti non sono validi, si prega di riprovare.');
+            document.getElementById('trip-kilometers').value = undefined;
+            document.getElementById('passenger-age').value = undefined;
+            outputTotalPrice = undefined;
+
+        } else {
+            outputTotalPrice.innerHTML = parseFloat(totalPrice.toFixed(2)) + ' ' + '€';
+        }
+        
+
 });
+
+
 
 
